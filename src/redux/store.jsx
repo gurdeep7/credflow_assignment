baf0def29d403dd0b0fd1a369b1e0f2051afa807
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2022-03-15 18:23:52 +0530

    Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,6 +3,6 @@ import { reducer } from "./reducer"
 const rootReducer = combineReducers({cartState:reducer})
 
 
-export const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__())
+export const store = createStore(rootReducer)
 
 console.log("entire State", store.getState())
